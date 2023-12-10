6c2420bf6def2c264c0993bfe4d45a4ee26b76fd
GhaznaviByte <ghaznavizahra77@gmail.com>
2023-12-10 02:48:49 -0800

    Update background and add weather app container
    - Remove background image and corresponding function
    - Add weather app container in HTML
    - Style weather app container in CSS

--- src/app.js
+++ src/app.js
@@ -1,20 +1,4 @@
-function updateBackgroundImage() {
-  const body = document.body;
-  const background = document.querySelector(".background");
-
-  if (body.classList.contains("dark")) {
-    // Dark mode is on, set dark background image
-    body.style.backgroundColor = "#333333";
-    background.style.backgroundImage = "url('../images/darkBackground.png')";
-  } else {
-    // Dark mode is off, set light background image
-    body.style.backgroundColor = "#EDE7F6";
-    background.style.backgroundImage = "url('../images/lightBackground.png')";
-  }
-}
-
 let switchToggle = document.getElementById("switch-toggle");
 switchToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  updateBackgroundImage();
 });
