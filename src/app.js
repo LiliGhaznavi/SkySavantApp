ed7904f0a955f3d3e344bed1b6e43b9f8536add8
GhaznaviByte <ghaznavizahra77@gmail.com>
2023-12-11 06:48:57 -0800

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-function searchCity(event) {
+function updateContent(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchButton = document.querySelector("#submit-input");
-searchButton.addEventListener("click", searchCity);
+searchButton.addEventListener("click", updateContent);
+
+function updateTemperature(response) {
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature"
+  );
+  let currentCityElement = document.querySelector("#current-city");
+
+  currentTemperatureElement.innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  currentCityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "a8b8d02a8ccd7d0ta93931494eef0o1b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateTemperature);
+}
 
 //Dark mode & Light mode switch toggle
 let switchToggle = document.getElementById("switch-toggle");
