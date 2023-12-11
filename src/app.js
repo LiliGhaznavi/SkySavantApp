a8c518156f8c329ff2f6409f1397e5cf4ecc2933
GhaznaviByte <ghaznavizahra77@gmail.com>
2023-12-11 04:47:31 -0800

    Add the basic search engine (no API)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = searchInput.value;
+}
+
+let searchButton = document.querySelector("#submit-input");
+searchButton.addEventListener("click", searchCity);
+
+//Dark mode & Light mode switch toggle
 let switchToggle = document.getElementById("switch-toggle");
 switchToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
